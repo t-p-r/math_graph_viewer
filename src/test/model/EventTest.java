640a4b1d84ce4848c7ebb32120390bb442322e79
t-p-r <quanthangthien1000@gmail.com>
2024-04-05 15:14:32 -0700

    small fixes

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -15,22 +15,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EventTest {
 	private Event e;
 	private Date d;
-	
-	//NOTE: these tests might fail if time at which line (2) below is executed
-	//is different from time that line (1) is executed.  Lines (1) and (2) must
-	//run in same millisecond for this test to make sense and pass.
-	
+
+	// NOTE: these tests might fail if time at which line (2) below is executed
+	// is different from time that line (1) is executed. Lines (1) and (2) must
+	// run in same millisecond for this test to make sense and pass.
+
 	@BeforeEach
 	public void runBefore() {
-		e = new Event("Sensor open at door");   // (1)
-		d = Calendar.getInstance().getTime();   // (2)
+		e = new Event("Sensor open at door"); // (1)
+		d = Calendar.getInstance().getTime(); // (2)
 	}
-	
-	 @Test
-	 public void testEvent() {
-	 	assertEquals("Sensor open at door", e.getDescription());
-	 	e.getDate();
-	 }
+
+	@Test
+	public void testEvent() {
+		assertEquals("Sensor open at door", e.getDescription());
+		assertEquals(e.getDate(), d);
+	}
 
 	@Test
 	public void testToString() {
